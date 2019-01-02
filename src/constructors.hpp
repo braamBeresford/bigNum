206c09a77f8876775ca80d98fb5a60684b7b257f
Braam <braam7beresford@gmail.com>
2019-01-02 11:31:35 -0800

    Constructors complete

--- src/constructors.hpp
+++ src/constructors.hpp
@@ -1,25 +1,19 @@
 #ifndef BIG_NUM_CONSTRUCTORS_HPP
 #define BIG_NUM_CONSTRUCTORS_HPP
 #include "BigNum.hpp"
-#include "math.h"
 #include <stdlib.h>
 
 BigNum::BigNum(){
 	data= new std::vector<uint_fast64_t>;
 };
 
-BigNum::BigNum(const long long& num){
-	BigNum();
+BigNum::BigNum(const long long& num):BigNum(){
 	data->push_back(num);
 };
 
-BigNum::BigNum(const std::string num){
-	data= new std::vector<uint_fast64_t>;
+BigNum::BigNum(const std::string num):BigNum(){
 	data->push_back(stoi(num));
 }
 
-// BigNum BigNum::operator+() const {
-//     return *this;
-// }
 
 #endif
