667bbe655607ad715877552e9dbbf8069011bf0f
Braam <braam7beresford@gmail.com>
2019-01-05 00:10:56 -0800

    IOStream complete

---
+++ src/operators/io_stream.hpp
@@ -0,0 +1,27 @@
+#ifndef IO_STREAM_HPP
+#define IO_STREAM_HPP
+
+
+/*
+    BigInt to output stream
+    -----------------------
+*/
+
+std::ostream& operator<<(std::ostream& out, const BigNum& num) {
+    if(num.sign == -1)
+    	out << '-';
+    for(int i = num.data->size()-1; i >=0 ; i--)
+    	out << num.data->at(i);
+
+    return out;
+}
+
+std::istream& operator>>(std::istream& in, BigNum& num) {
+    std::string input;
+    in >> input;
+    num = BigNum(input);
+    return in;
+}
+
+
+#endif
